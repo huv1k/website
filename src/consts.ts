b5e0865a1b92a10f07db747febcb8aa41ef67a88
Lukáš Huvar <lukas@huvar.cz>
2026-01-27 09:50:06 +0100

    fix: shorten title tag to under 60 characters for SEO

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Huvik - Software Developer | React, GraphQL & TypeScript Expert";
+export const SITE_TITLE = "Huvik - React, GraphQL & TypeScript Developer";
 export const SITE_DESCRIPTION =
   "Personal site by Lukáš Huvar. A software developer from the Czech Republic. Passionated about React, GraphQL, Typescript.";
